700ec87a351ebca6d56d1eb0a152d5041db1ddd9
spectra-guys <69073757+spectra-guys@users.noreply.github.com>
2020-08-17 16:33:39 +0530

    commit

---
+++ stream_to_youtube.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# Diffusion youtube avec ffmpeg
+
+# Configurer youtube avec une résolution 720p. La vidéo n'est pas scalée.
+
+VBR="2500k"                                    # Bitrate de la vidéo en sortie
+FPS="30"                                       # FPS de la vidéo en sortie
+QUAL="medium"                                  # Preset de qualité FFMPEG
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"  # URL de base RTMP youtube
+
+SOURCE="/coy.mp4"              # Source UDP (voir les annonces SAP)
+KEY="1qd7-8d7d-peda-62xc-egmh"                                     # Clé à récupérer sur l'event youtube
+
+ffmpeg \
+    -stream_loop -1 -i "$SOURCE" -deinterlace \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
+    -f flv "$YOUTUBE_URL/$KEY"
